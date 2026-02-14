04a79b658c21696933fb466ea8d03f099925b6bb
Gummadala vikas <vikas93912@gmail.com>
2026-02-14 11:26:00 +0530

    Fix redirects file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { viteStaticCopy } from "vite-plugin-static-copy"
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects",
+          dest: "."
+        }
+      ]
+    })
+  ]
 })
-
